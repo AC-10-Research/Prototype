8be50b188a8dd7e0681beaaac73a3d893abaf865
AC-10-Research <mzboinsk@students.kennesaw.edu>
2024-10-21 09:37:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-import random
-import collections
-import glob
-
 import numpy as np
-from numpy.matlib import repmat
 import tensorflow as tf
+from numpy.matlib import repmat
+from tensorflow.keras.layers import Dense, SimpleRNN
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, SimpleRNN, LSTM
-import pandas as pd
-
 import pretty_midi
-#import fluidsynth
+import fluidsynth
 
 # dataset parameters
 copies = 200       # number of times to copy the song
@@ -20,6 +14,22 @@ nn_nodes = 10      # number of nodes in the RNN
 # training parameters
 epochs = 200         # number of epochs used for training
 batch_size = None   # size of each batch (None is default)
+
+def create_midi_from_predictions(predictions, output_file='output.mid', instrument_program='0'):
+    midi_data = pretty_midi.PrettyMIDI()
+    instrument = pretty_midi.Instrument(program=instrument_program)
+    start_time = 0.0
+    duration = 0.5
+
+    for note in predictions:
+        note_number = int(note)
+        midi_note = pretty_midi.Note(velocity=100, pitch=note_number, start=start_time, end=start_time + duration)
+        instrument.notes.append(midi_note)
+        start_time += duration
+
+    midi_data.instruments.append(instrument)
+    midi_data.write(output_file)
+    print(f'MIDI file saved as: {output_file}')
 
 def dataset_from_song(song,copies,window_length):
     # repeat song "copies" times
@@ -42,7 +52,7 @@ def dataset_from_song(song,copies,window_length):
 
     return x_train,y_train
 
-# a scale
+#  Middle C Scale
 song = np.array([72,74,76,77,79,81,83,84])
 # generate a dataset from copies of the song
 x_train,y_train = dataset_from_song(song,copies,window_length)
@@ -84,3 +94,11 @@ predictions = model.predict(x_test).round()
 correct = sum(predictions == y_test)
 N = len(predictions)
 print(" test set accuracy: %.4f%% (%d/%d)" % (100*correct/N,correct,N))
+create_midi_from_predictions(predictions, output_file='predicted_song.mid', instrument_program=0)
+
+pm = pretty_midi.PrettyMIDI('predicted_song.mid')
+def display_audio(pm: pretty_midi.PrettyMIDI, seconds = 30):
+    waveform = pm.fluidsynth(fs=_SAMPLING_RATE)
+    waveform_short = waveform[:seconds*_SAMPLING_RATE]
+    return display.Audio(waveform_short, rate=_SAMPLING_RATE)
+display_audio(pm)
